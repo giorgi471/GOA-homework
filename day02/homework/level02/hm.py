8b161e685d21d25b43eb2bffcab05adb7a0ce403
giorgi741 <giorgimanjgaladze28@gmail.come>
2025-07-06 15:23:24 +0400

    second commit

---
+++ day02/homework/level02/hm.py
@@ -0,0 +1,42 @@
+A1 = 20
+A2 = 80
+print(20 + 80, 80 - 20, 80 * 20, 80 / 20)
+
+
+
+A1 = "saxli"
+A2 = "manqana"
+A3 = "skami"
+A4 = "adamiani"
+A5 = 12
+A6 = 400
+A7 = 573
+A8 = 671
+A9 = 2.9
+A10 = 1234.5
+A11 =764.8
+A12 =954.1
+print(A1,A2,A3,A4,A5,A6,A7,A8,A9,A10,A11,A12)
+
+
+A1 = "deda"
+A2 = "miwa"
+print(A1 + A2)
+#ამ მოქმედებბას ქვია "კონკატინაცია"
+
+
+A1 = "me"
+A2 = "vswavlob"
+A3 =  "programirebis"
+A4 = "sauketeso"
+A5 = "skolashi"
+print(A1 + A2 + A3 + A4 + A5)
+
+
+A1 = 200.2
+A2 = 120
+A3 = 320.2    #(A1+A2)
+A4 = 80.2     #(A1-A2)
+A5 = 24024    #(A1*A2)
+
+
