e3e3104650e09e32a37f510747e09e52853162cb
giorgi741 <giorgimanjgaladze28@gmail.come>
2025-07-09 17:04:17 +0400

    second commit

---
+++ day03/classwork/cls.py
@@ -0,0 +1,15 @@
+A1 = 284
+A1 = 4002
+print(A1)
+giorgi = "name"
+Giorgi ="name"
+GiOrGi = "name"
+giorGI = "name"
+gIorGi ="name"
+A1_gio = "gio", "sandro", "data", "saba"
+A2_gio = 12, 134, 456, 745
+A3_gio = 124,72 , 262172,2 , 637,91 , 142,1
+user_name = "goga" #sxva rame ewera da unda dagvewera qvedatire
+user_surname = "chalauri" #aklda qvedatire
+first-number = 200 #aqac aklda qvedatire
+second_number = 123 #aq sxva rame ewera da unda shegvecvala qvedatirit
---
+++ day03/homework/hmw.py
@@ -0,0 +1,36 @@
+A1_gio = 9123
+A2_gio = 47937
+A3_gio = A1_gio + A2_gio, A1_gio * A2_gio, A1_gio / A2_gio, A1_gio - A2_gio
+
+A1 = 645735
+A1 = "gio"
+A1 = 773944
+A1 = 28,9
+A1 = "davaleba"
+a1 = "sarke"
+print(sarke)
+
+name = "gio"
+nAme = "vato"
+Name = "data"
+naMe = "sandro"
+namE = "saba"
+print = (name, nAme, Name, naMe, namE)
+
+name = "giorgi"         #cvladis daweris dros ar sheidzleba mas win qondea ricxvi
+user_name = "bubunauri"  #aq shuashii iyo araswori simbolo dasmuli
+user_name = "goga"        #aklia intejeri
+user-surname = "axalaia"  #aklia intejeri
+
+A_1 = "magidaze"
+A_2 = "saxazavi"
+A_3 = "sashleli"
+A_4 = "da"
+A_5 = "wignia"
+print(A_1 + A_2 + A_3 + A_4 + A_5)
+
+A1 = "gio"
+A2 = 10
+print(A1 * A2)
+
+#ჩვენ არ შეგვიძლია სტრინგის და ონტეჯერის მიმატება, გამოკლება და გაყოფა ამ მოქმედებას ტერმინალი აღიქვავს როგორც შეცომს. ჩვენ მხოლოდ შეგვიძლია მათი ერტმანეთზე გამარავლეება.
