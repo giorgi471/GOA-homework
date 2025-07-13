fe7a11e630b4a3fba02859ad8f0ae74feb9c9004
giorgi741 <giorgimanjgaladze28@gmail.come>
2025-07-13 12:50:00 +0400

    second commit

---
+++ day04/claswork/clw.py
@@ -0,0 +1,15 @@
+name = input("gamarjoba ra gqvia:")
+print(name)
+
+input("4")
+input("2")
+print(int("4" + "2"))
+
+input("14")
+print(int("14"))
+
+A1 = "gio"
+A2 = 14
+A3 = 1.77
+A4 = False
+print(A1, A2, A3, A4)
---
+++ day04/homework/homw.py
@@ -0,0 +1,35 @@
+#input- არის ფუნქცია რომელიც გვაძლევს სშვალებას შვიტანოთ ტერმინალში რაიმე მნიშვნელობა
+#output- არის ფუნქცია რომელიც პასუხობს მომხმარებლისთვის რაღაც მნიშვნელობის სახით
+
+name = input("gamarjoba ra gqvia :")
+print(type(str(name)))
+print(name)
+
+A1 = "gio"    #str
+A2 = 53       #int
+A3 = 5835.374    #float
+ 
+A1 = "gio"
+A2 = 345
+A3 = 362.64
+print(type(A1))
+print(type(A2))
+print(type(A3))  
+
+A1 = input("gamarjoba")
+A2 = input("rogor xar")
+print(A1 + A2)
+
+input("2")
+input("65")
+input("64")
+input("84")
+input("674")
+print(2 + 65 + 65 + 84 + 674)
+print(890 / 5)
+
+A1 = "gio"
+A2 = "14"
+A3 = "1.77"
+A4 = "58.7"
+print(A1 + A2 + A3 + A4)
