9ee8b97c1d95dfa43b0b31bddf0a2d1dc9c5aa25
giorgi741 <giorgimanjgaladze28@gmail.come>
2025-07-27 15:59:46 +0400

    second commit

---
+++ DAY08/classwork/cls.py/cls.py
@@ -0,0 +1,9 @@
+print(10<31 or 284==284)
+print(50>23 or 2344==546)
+print(35>12 or 21==12)
+print(12==12 and 48<34)
+print(122>12 and 364==21)
+print(3452==3452 and 3948>7645746)
+
+for i in range(20):
+    print("gio")
