7e096ccd58ea5f969849e598ec6e0e3bb759bae2
giorgi741 <giorgimanjgaladze28@gmail.come>
2025-07-28 16:45:55 +0400

    second commit

---
+++ day06/classwork/cls.py
@@ -0,0 +1,39 @@
+#logical operation_aris logikuri operaciebi romelsac viyenabt kodshi rata gaviogot is martalia tu mcdari
+
+A1 = (input("shemoitane saxeli"))
+A2 = (input("shemoitane saxeli"))
+print(A1==A2)
+
+print(12>12 or 2<23)
+print(123>12 or 464<936)
+print(3513251==5623 and 863862>29892)
+print(1213224545==7123 and 82374<863)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ day06/homework/hmw.py
@@ -0,0 +1,67 @@
+A1 = (input("shemoitane ricxvi"))
+A2 = (input("shemoitane ricxvi"))
+print(A1>A2)
+print(A1<A2)
+print(A1==A2)
+
+A1 = int(input("shemoitane ricxvi"))
+A2 = int(input("shemoitane ricxvi"))
+A3 = int(input("shemoitane ricxvi"))
+A4 = 7
+A5 = 2
+A6 = A1*A2*A3*A4*A5
+print(A6/5)
+
+A1 = (input("semoitane stringi"))
+A2 = (input("shemoitane stringi"))
+A3 = (input("shemoitane stringi"))
+A4 = (input("shemoitane intejeri"))
+A5 = A1+A2+A3
+print(A5+A4)
+
+#chven viswavlet or da and operatorevi or operatoris dros erti tu mainc sworia mashin tviton sworia da to end shemtxvevashi erti mainc mcdaria mashin magaliti mcdaria
+
+#(and)                             (or)
+#True and True tru-- ...      |   True or True --true ...           
+#True and False -- fals...     |   True or False -- true...
+#False and True -- fals...     |   False or True -- true...
+#False and False -- fals...    |   False or False -fals
+
+True or False
+False and True
+
+A1 = type(input("shemoitane stringi"))
+A2 = type(input("shemoitane intejeri"))
+A3 = type(input("shemoitane floati"))
+A4 = type(input("shemoitane boolian"))
+
+A1 = str(input("shemoitane stringi"))
+A2 = int(input("shemoitane intejeri"))
+A3 = float(input("shemoitane floati"))
+
+print(A1)
+print(A2)
+print(A3)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
