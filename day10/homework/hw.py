1b38dbd85514e1620db96c0fe83ce3f38d1340b9
giorgi741 <giorgimanjgaladze28@gmail.come>
2025-08-03 15:22:32 +0400

    second commit

---
+++ day10/homework/hw.py
@@ -0,0 +1,59 @@
+for i in range(0,39):
+    print(i)
+
+for i in range(30,40):
+    print(i)
+
+for i in range(12,120):
+    print(i)
+
+for i in range(19):
+    print("giorgi  14")
+
+for i in range(10,100,3):
+    print(i)
+
+for i in range(0,31):
+   print(str(i)+"giorgi")
+   
+for i in range(14):
+    print(i)
+    (input("ra gqvia"))
+
+i = 0
+while 1<10:
+    print(i)
+    i = i + 1
+
+i = 0
+while 1<24:
+    print(i)
+    i = i + 1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
