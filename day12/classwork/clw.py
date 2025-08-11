a2eec21708a259b6d3a82d869347bbda16f3cce4
giorgi741 <giorgimanjgaladze28@gmail.come>
2025-08-11 14:39:12 +0400

    second commit

---
+++ day12/classwork/clw.py
@@ -0,0 +1,17 @@
+for i in range(11):
+    print(i)
+
+for i in range(5, 20):
+    print(i)
+
+
+#conditional statement- არის პირობითი განცხადება მაგალითად, თუ მომხმარებლის ასაკი მეტია ან ტოლია 18 ვამბობთ რომ ის სრულწლოვანია სხვა შემთხვევაში  არა
+
+A1 = int(input("shemoitane asaki"))
+
+age = A1
+if age >= 18:
+    print("srulwlovani xar")
+else:
+    print(" patara xar")
+
---
+++ day12/homework/hmw.py
@@ -0,0 +1,47 @@
+#conditional statement- არის პირობითი განცხადება მაგალითად, თუ მომხმარებლის ასაკი მეტია ან ტოლია 18 ვამბობთ რომ ის სრულწლოვანია სხვა შემთხვევაში  არა
+
+for i in range(51):
+    print("hallo world")
+
+for i in range(3,18):
+    print(i)
+
+A1 = int(input("shemoitane paroli"))
+password = A1
+if password >= 1234:
+    print("Password is correct")
+else:
+    print("Password is incorrect")
+
+
+A1 = (input("shemoitane cxoveli"))
+cxoveli= A1
+if cxoveli >= "dog":
+    print("woaf! woaf!")
+else:
+    print("შენ არ გყავს ძაღლი")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
