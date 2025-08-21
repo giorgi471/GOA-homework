c34b136de2c81179167eac531d974c2975ae62fb
giorgi741 <giorgimanjgaladze28@gmail.come>
2025-08-21 12:13:35 +0400

    second commit

---
+++ day15/homework/hw.py
@@ -0,0 +1,47 @@
+A1 = int(input("shemoitane ricxvi"))
+ricxvi = A1
+if ricxvi >= 10:
+    print("more than 10" )
+else:
+    print("less than 10")
+
+
+A1 = int(input("shemoitane ricxvi"))
+ricxvi = A1
+if ricxvi <= 15:
+    print( "equal to 15" )
+else:
+    print("not equal to 15")
+
+
+
+A1 = (input("shemoitane stringi"))
+stringi = A1
+if stringi <= "giorgi":
+    print("you are correct")
+else:
+    print("you are wrong")
+
+
+
+for i in range(50,100,5):
+    print(i)
+
+
+for i in range(15):
+    print("giorgi manjgaladze")
+
+
+
+i = 20
+while i < 50:
+    print(i)
+    i = i +1
+
+
+
+
+
+
+
+
