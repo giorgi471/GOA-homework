7ab9adc69ff46cbbe12e0d16d3cc4e9e2f3f4759
giorgi741 <giorgimanjgaladze28@gmail.come>
2025-09-01 12:48:45 +0400

    second commit

---
+++ day18/homework/mw.py
@@ -0,0 +1,71 @@
+A1 = (input("shemoitane ricxvi"))
+A2 = (input("shemoitane ricxvi"))
+
+
+
+if A1 > A2 :
+    print("first is more than second")
+
+elif A1 < A2 :
+    print("first is less than second")
+
+else:
+    print("first number equal to second number")
+
+
+
+
+
+A1 = (input("shemoitane sheni saxeli"))
+
+A2 = "giorgi"
+
+if A1 == A2 :
+    print("sexniebi vart")
+else :
+    print("sxvadasxva saxelebi gvaq")
+
+
+
+
+A1 = int(input("shemoitane ricxvi"))
+A2 = int(input("shemoitane ricxvi"))
+
+
+
+if A1 > 0 and A2 > 0:
+    print("ორივე რიცხვი დადებითია")
+elif A1 < 0 and A2 < 0:
+    print("ორივე რიცხვი არის უარყოფითი")
+else:
+    print("ეს რა ჯანდაბაა")
+
+
+
+for i in range(50,100,2):
+    print(i)
+
+
+
+
+i = 20
+while i <= 40:
+    print(i)
+    i + 1 == i
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
