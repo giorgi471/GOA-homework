75120ae177698c0e14883111becbefcb334c18f1
giorgi741 <giorgimanjgaladze28@gmail.come>
2025-10-09 11:37:43 +0400

    second commit

---
+++ day28/homework/hw.py
@@ -0,0 +1,58 @@
+a = [1,2,3,4,5,6,7,8,9,10]
+print(a[:5])
+print(a[-3:])
+print(a[3:8])
+
+
+b = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+print(b[:-9])
+print(b[-10:])
+print(b[-8:])
+
+
+w = "თვითმფრინავი"
+print(w[8:])
+
+
+t = "Programmer"
+print(t[:3])
+print(t[:-7])
+
+
+days = ["ორშაბათი","სამშაბათი","ოთხშაბათი","ხუთშაბათი","პარასკევი","შაბათი","კვირა"]
+print(days[:5])
+print(days[5:])
+print(days[:-2])
+print(days[-2:])
+
+
+s = "Python slicing is very powerful"
+print(s[:6])
+print(s[7:14])
+print(s[22:])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ day29/homework/hw.py
@@ -0,0 +1,65 @@
+text = "PythonProgramming"
+print(text[:6])
+
+
+
+
+numbers = [10, 20, 30, 40, 50, 60, 70]
+print(numbers[2:5])
+print(numbers[-5:-2])
+
+
+
+word = "HelloWorld"
+print(word[:5])
+print(word[:-5])
+
+
+
+
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g']
+print(letters[1], letters[3], letters[5])
+print(letters[-6], letters[-4], letters[-2])
+
+
+
+info = "Information"
+print(info[2:7])
+print(info[-9:-4])
+
+
+
+
+text2 = "abcdefghijklmno"
+print(text2[:4])
+print(text2[9:])
+print(text2[5:10])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
