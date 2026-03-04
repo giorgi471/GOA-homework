ccf4ed6bddb0e35ffb8e235e16bf38539074d49b
giorgi741 <giorgimanjgaladze28@gmail.come>
2026-03-04 23:44:22 +0400

    second commit

---
+++ day69/homework/hw.py
@@ -0,0 +1,48 @@
+#def solution(string):
+    # return string[::-1]
+
+
+
+
+
+
+# def positive_sum(arr):
+#     total = 0
+#     for i in arr:
+#         if i > 0:
+#             total += i
+#     return total
+
+
+
+
+
+
+
+
+# def repeat_str(repeat, string):
+#     return repeat * string
+
+
+
+
+
+
+# def square_sum(numbers):
+#     total = 0
+#     for i in numbers:
+#         total += i * i
+#     return total
+
+
+
+
+
+
+
+# def string_to_number(s):
+#     return int(s)
+
+
+
+
---
+++ day70/homework/hw.py
@@ -0,0 +1,80 @@
+# def greet():
+#     return "hello world!"
+
+
+
+
+
+
+
+
+
+# def summation(num):
+#     total = 0
+#     for i in range(1, num + 1):
+#         total += i
+#     return total
+
+
+
+
+
+
+
+# def sum_array(a):
+#     total = 0
+#     for i in a:
+#         total += i
+#     return total
+
+
+
+
+
+
+
+
+
+
+
+#?
+
+
+
+
+
+
+
+
+
+# def areYouPlayingBanjo(name):
+#     if name[0].lower() == 'r':
+#         return name + ' plays banjo'
+#     else:
+#         return name + ' does not play banjo'
+
+
+
+
+
+
+
+
+# def abbrev_name(name):
+#     space_index = name.index(" ")
+#     return name[0].upper() + "." + name[space_index + 1].upper()
+
+
+
+
+# ?
+
+# ?
+
+
+
+
+
+
+
+
