22885e95757f944c17fe6f51126e5bb9cb5ef5c6
giorgi741 <giorgimanjgaladze28@gmail.come>
2026-03-11 15:12:13 +0400

    second commit

---
+++ day73/homework/hw.py
@@ -0,0 +1,50 @@
+#1
+#  def string_clean(s):
+#     for i in "0123456789":
+#         s = s.replace(i, "")
+#     return s
+
+
+
+
+
+
+
+
+
+#3
+#  def quote(fighter):
+#     fighter_lower = fighter.lower()
+    
+#     if fighter_lower == "george saint pierre":
+#         return "I am not impressed by your performance."
+#     elif fighter_lower == "conor mcgregor":
+#         return "I'd like to take this chance to apologize.. To absolutely NOBODY!"
+#     else:
+#         return "Unknown fighter"
+
+
+
+
+
+#4
+#  def replace_exclamation(st):
+#     vowels = "aeiouAEIOU"
+#     for i in vowels:
+#         st = st.replace(i, "!")
+#     return st
+            
+
+
+
+# 5
+# def stringy(size):
+#     result = ""
+#     for i in range(size):
+#         if i % 2 == 0:
+#             result += "1"
+#         else:
+#             result += "0"
+#     return result
+
+
