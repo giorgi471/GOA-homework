307dfc80ab78278765687c361dcbaab53e1a65e3
giorgi741 <giorgimanjgaladze28@gmail.come>
2026-04-16 11:23:36 +0400

    second commit

---
+++ day82/homework/hw.py
@@ -0,0 +1,87 @@
+# def spin_words(sentence):
+#     words = sentence.split()
+#     result = []
+
+#     for word in words:
+#         if len(word) >= 5:
+#             result.append(word[::-1])
+#         else:
+#             result.append(word)
+
+#     return " ".join(result)
+
+
+
+
+
+
+
+# def capitalize(s):
+#     even = ""
+#     odd = ""
+
+#     for i in range(len(s)):
+#         if i % 2 == 0:
+#             even += s[i].upper()
+#             odd += s[i]
+#         else:
+#             even += s[i]
+#             odd += s[i].upper()
+
+#     return [even, odd]
+
+
+
+
+
+
+
+
+# def reverse_words(text):
+#     words = text.split(" ")
+#     result = []
+
+#     for word in words:
+#         result.append(word[::-1])
+
+#     return " ".join(result)
+
+
+
+
+
+
+# def vowel_indices(word):
+#     vowels = "aeiouyAEIOUY"
+#     result = []
+
+#     for i in range(len(word)):
+#         if word[i] in vowels:
+#             result.append(i + 1)  
+
+#     return result
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
