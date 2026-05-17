d33c978255a3fa2806868fc67971552d4e539f36
giorgi741 <giorgimanjgaladze28@gmail.come>
2026-05-17 23:58:48 +0400

    second commit

---
+++ day92/homework/hw.py
@@ -0,0 +1,72 @@
+#1   ?
+
+#2
+# def palindrome_chain_length(n):
+#     steps = 0
+
+#     while str(n) != str(n)[::-1]:
+#         n = n + int(str(n)[::-1])
+#         steps += 1
+
+#     return steps
+
+
+
+
+# 3
+# def nth_smallest(arr, pos):
+#     return sorted(arr)[pos - 1]
+
+
+
+
+
+# 4
+# def solution(start, finish):
+#     distance = finish - start
+#     return distance // 3 + distance % 3
+
+
+
+# 5
+# def incrementer(nums):
+#     result = []
+
+#     for i in range(len(nums)):
+#         result.append((nums[i] + i + 1) % 10)
+
+#     return result
+
+
+
+
+
+
+#6   ?
+
+
+
+
+
+# 7
+# def largest(n, xs):
+#     xs.sort()
+#     return xs[len(xs)-n:]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
