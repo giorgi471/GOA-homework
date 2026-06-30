1a5973141510d16c76e9fb6eb7b1dec0a58965e9
giorgi741 <giorgimanjgaladze28@gmail.come>
2026-06-30 14:35:09 +0400

    first commit

---
+++ day101/homework/hw.py
@@ -0,0 +1,47 @@
+#1   ?
+
+
+# 2 
+# def break_chocolate(n, m):
+#     if n <= 0 or m <= 0:
+#         return 0
+    
+#     return n * m - 1
+
+
+
+# 3
+# def is_anagram(test, original):
+#     return sorted(test.lower()) == sorted(original.lower())
+
+
+
+
+# 4
+# def over_the_road(address, n):
+#     return (2 * n + 1) - address
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
