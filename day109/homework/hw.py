c965493b87d0f234b75b8687fa4bf1ccedfaf5ce
giorgi741 <giorgimanjgaladze28@gmail.come>
2026-07-25 18:14:09 +0400

    first commit

---
+++ day109/homework/hw.py
@@ -0,0 +1,44 @@
+# 1
+# def add_binary(a, b):
+#     n = a + b
+
+#     if n == 0:
+#         return "0"
+
+#     binary = ""
+
+#     while n > 0:
+#         binary = str(n % 2) + binary
+#         n //= 2
+
+#     return binary
+
+
+# 2
+# def interlockable(a, b):
+#     return (a & b) == 0
+
+
+
+
+# 3   ?
+
+
+# 4  ?
+
+# 5
+# def row_sum_odd_numbers(n):
+#     return n ** 3
+
+
+
+
+
+
+
+
+
+
+
+
+
