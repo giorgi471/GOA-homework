7489551c034c4b977c660886ed35700476b1d9c2
giorgi741 <giorgimanjgaladze28@gmail.come>
2026-09-25 20:44:48 +0400

    second commit

---
+++ day129/classwork/hw.js
@@ -0,0 +1,77 @@
+
+
+// let num = Number(prompt("შეიყვანე რიცხვი:"))
+
+// switch (true) {
+//     case num > 0 && num % 2 === 0:
+//         console.log("positive even")
+//         break
+
+//     case num > 0 && num % 2 !== 0:
+//         console.log("positive odd")
+//         break
+
+//     case num < 0 && num % 2 === 0:
+//         console.log("negative even")
+//         break
+
+//     case num < 0 && num % 2 !== 0:
+//         console.log("negative odd")
+//         break
+
+//     default:
+//         console.log("0")
+// }
+
+
+
+
+
+// function sayMyInfo() {
+//     console.log("სახელი: გიორგი")
+//     console.log("გვარი: მანჯგალაძე")
+//     console.log("ასაკი: 15")
+//     console.log("მისამართი: თბილისი")
+// }
+
+// sayMyInfo()
+// sayMyInfo()
+// sayMyInfo()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function sayInfo(name, surname, parchusPrice) {
+    console.log("Hello my name is " + name + " my surname is " + surname + " and parchusPrice is " + parchusPrice + "!")
+}
+
+sayInfo("Giorgi", "Manjgaladze", 100)
+sayInfo("Nika", "Beridze", 250)
+sayInfo("Luka", "Kapanadze", 500)
+
+
+
+
+
+
+
+
+
+
+
